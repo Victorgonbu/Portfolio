2513fa5c8e096816b7a4916883e62fc5e39592dd
victorG <peter200970@hotmail.com>
2021-03-13 00:39:29 -0500

    Split style for different screen size and add dropdown menu for phone viewport

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,2 +1,19 @@
 import '../style/reset.css';
-import '../style/main.css';
+import '../style/small.css';
+import '../style/medium.css';
+import '../style/big.css';
+
+const dropdown = document.querySelector('[data-dropdown]');
+const dropdownMenu = document.querySelector('[data-dropdown-menu]');
+
+dropdown.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('active');
+});
+
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('active');
+    });
+});
