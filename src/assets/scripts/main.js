2956f8f1adc28da84d4484c3e597a23b9da989d3
victorG <peter200970@hotmail.com>
2021-03-17 02:43:24 -0500

    Fix eslint errors

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,8 +2,7 @@ import '../style/reset.css';
 import '../style/small.css';
 import '../style/medium.css';
 import '../style/big.css';
-import gsap from 'gsap';
-import Dropdown from './dropdown'
+import Dropdown from './dropdown';
 import Modal from './modal';
 
 const dropdown = document.querySelector('[data-dropdown]');
@@ -11,14 +10,13 @@ const navLink = document.querySelectorAll('.nav-link');
 
 dropdown.addEventListener('click', Dropdown.toggleFold);
 
-navLink.forEach(link => { link.addEventListener('click', Dropdown.fold) });      
+navLink.forEach(link => { link.addEventListener('click', Dropdown.fold); });
 
 const seeProjectButtons = document.querySelectorAll('[data-modal-target]');
 
 
-seeProjectButtons.forEach( project => { project.addEventListener('click', () => { Modal.open(project) }) });
+seeProjectButtons.forEach(project => { project.addEventListener('click', () => { Modal.open(project); }); });
 
 const projectImages = document.querySelectorAll('[data-image-target]');
 
-projectImages.forEach(image => { image.addEventListener('click', () => { Modal.setHeroImage(image) }) });
-
+projectImages.forEach(image => { image.addEventListener('click', () => { Modal.setHeroImage(image); }); });
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
 module.exports = {
-    entry: {
-        main: './src/assets/scripts/main.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src/'),
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
-            },
-        
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-        })
-    ]
-}
+  entry: {
+    main: './src/assets/scripts/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+};
